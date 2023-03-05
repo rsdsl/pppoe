6a76ddf414cdb5b86eb01a3f1579782a8e64d5b6
HimbeerserverDE <himbeerserverde@gmail.com>
2023-03-05 14:46:21 +0100

    send PADI

---
+++ src/error.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("usage: missing interface argument")]
+    MissingInterface,
+    #[error("bytes transmitted is not equal to request size")]
+    PartialRequest,
+    #[error("io error")]
+    Io(#[from] io::Error),
+    #[error("pppoe error: {0:?}")]
+    Pppoe(pppoe::error::Error),
+    #[error("pppoe parse error: {0:?}")]
+    PppoeParse(pppoe::error::ParseError),
+}
+
+impl From<pppoe::error::Error> for Error {
+    fn from(err: pppoe::error::Error) -> Self {
+        Self::Pppoe(err)
+    }
+}
+
+impl From<pppoe::error::ParseError> for Error {
+    fn from(err: pppoe::error::ParseError) -> Self {
+        Self::PppoeParse(err)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use rsdsl_pppoe::error::{Error, Result};
+
+use std::env;
+
+use pppoe::header::HeaderBuilder;
+use pppoe::packet::PPPOE_DISCOVERY;
+use pppoe::socket::Socket;
+use pppoe::tag::Tag;
+
+const MAC_BROADCAST: [u8; 6] = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
+
+fn main() -> Result<()> {
+    let link = env::args().nth(1).ok_or(Error::MissingInterface)?;
+
+    let host_uniq = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    let socket = Socket::on_interface(&link)?;
+    let local_mac = socket.mac_address();
+
+    let mut discovery = [0; 14 + 6 + 4 + 20];
+    let mut discovery_header = HeaderBuilder::create_padi(&mut discovery[14..])?;
+
+    discovery_header.add_tag(Tag::ServiceName(b""))?;
+    discovery_header.add_tag(Tag::HostUniq(&host_uniq))?;
+
+    new_broadcast_packet(local_mac, &mut discovery)?;
+
+    println!("sending PADI");
+
+    let n = socket.send(&discovery)?;
+    if n != discovery.len() {
+        return Err(Error::PartialRequest);
+    }
+
+    Ok(())
+}
+
+fn new_broadcast_packet(local_mac: [u8; 6], buf: &mut [u8]) -> Result<()> {
+    let mut ethernet_header = pppoe::eth::HeaderBuilder::with_buffer(&mut buf[..14])?;
+
+    ethernet_header.set_src_address(local_mac);
+    ethernet_header.set_dst_address(MAC_BROADCAST);
+    ethernet_header.set_ether_type(PPPOE_DISCOVERY);
+
+    Ok(())
 }
